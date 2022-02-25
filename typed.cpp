1de9a4aa49015770e5152622cc11914888316a28
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-02-25 18:29:05 +0530

    Add files via upload

---
+++ typed.cpp
@@ -0,0 +1,53 @@
+// Implementation of Type Conversion  Default constructor //
+// Convert basic number to time type //
+// Time t1 = 120; ---> t1.hour = 2, t1.min = 0; //
+
+#include<iostream>
+using namespace std;
+
+class Time
+{
+    int hour, min;
+    
+    public:
+       
+       Time()   // default constructor
+       {
+                cout<< "default const\n";
+                hour = 0;
+                min =  0;
+       }
+       
+       Time(int x) // param constructor
+       {
+          cout << "Param const\n";
+          hour = x / 60;
+          min  = x % 60;
+       }
+       
+//       void operator = (int x)
+//       {
+//          cout<< "overload operator '=' \n";
+//          hour = x / 60;
+//          min  = x % 60;
+//       }
+                 
+       void display()
+       {
+          cout<< " Time is : " << hour << "hr and " << min << " min" <<  endl;
+       }   
+
+};
+
+
+int main()
+{
+       int j = 55;
+       Time t1;
+//       t1 = j;
+       t1.display();
+   
+       return 0;
+}                     
+                    
+          
