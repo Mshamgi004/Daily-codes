37272709ee22a39fb8ba03142fa80e982a47bae0
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-03-24 18:48:51 +0530

    Add files via upload

---
+++ substring.cpp
@@ -0,0 +1,28 @@
+// Substring Class work //
+// Mahadev G I //
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+   char s[100];
+   int i;
+   
+   cout << "Enter a string\n";
+   cin >> s ;
+   
+   for(int i=0; i < s.end() ; i++)
+   {
+      if(i == s.end())
+      {
+         cin >> s.end();
+      }
+      else
+      {
+            
+      
+      
+   
+   
+    
