430a48652c34434a3ff4321192e360e6ca10e4a4
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-03-31 14:21:43 +0530

    Add files via upload

--- messaget.c
+++ messaget.c
@@ -18,17 +18,21 @@ int main()
    int qid;
    int i;
    msg m1,m2;
+   
    qid = msgget(32,IPC_CREAT|0644);
+   
    m1.msgtype=10;
    
    for(i=0;i<100;i++)
       m1.buffer[i]='a';
       m2.msgtype=20;
+   
    for(i=0;i<100;i++)
       m2.buffer[i]='b';
    
    i = msgsnd(qid,&m1,sizeof(msg),0);
    printf("Return value of msgsnd of as= %d\n",i);
+   
    i = msgsnd(qid,&m2,sizeof(msg),0);
    printf("Return of msgsnd of bs= %d\n",i);
    
