9239d4199a956e3cbac1c0feb32e473605e72e7f
Mahadev G I <98145598+Mshamgi004@users.noreply.github.com>
2022-04-09 00:33:36 +0530

    Add files via upload

---
+++ mess_rx.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define key 34
+
+struct msgqu
+{
+	long mytype;
+	char data[512];
+};
+
+int main()
+{
+	int id;
+	struct msgqu q1, q2;
+	struct msqid_ds mybuff;
+
+	id = msgget(key, IPC_CREAT|0644);              // Creating a id using msgget function
+	if(id < 0)
+	{
+		perror("msgget");
+		exit(EXIT_FAILURE);
+	}
+	printf("Message queue id: %d\n",id);
+
+	
+	msgctl(id, IPC_STAT, NULL);                          //  msgctl function to print the number of messages
+
+	
+
+        // printing contents of message queue through struct msgid_ds
+	
+        printf("Contents from the struct are: \n");
+	printf("no of msges in queue: %d\n",mybuff.msg_qnum);
+	printf("no of bytes in queue: %d\n",mybuff.msg_cbytes);
+	printf("max no of bytes in queue: %d\n",mybuff.msg_qbytes);
+
+	
+	msgrcv(id, &q1, sizeof(q1), 5, 0);                     // msgrcv function used to recieve the type1 of message
+
+	printf("Type1 is: %ld\n",q1.mytype);
+	printf("Message1 is: %s\n",q1.data);
+
+	msgrcv(id, &q2, sizeof(q2), 7, 0);                    // msgrcv function used to recieve the type2 of message
+
+	printf("Type2 is: %ld\n",q2.mytype);
+	printf("Message2 is: %s\n",q2.data);
+	
+        msgctl(id, IPC_RMID, NULL);                           // msgctl function used to print the messages in type1 and type2 
+
+	return 0;
+}
